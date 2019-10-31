1a7c978f482e06b97a3484c72e94309a172a57e5
JP Bourget <jp@syncurity.net>
2019-10-30 20:55:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ if sys.version_info <= (2, 4):
     print >> sys.stderr, error
     sys.exit(1)
 
-
 requirements = [
     'certifi==2017.7.27.1',
     'chardet==3.0.4',
@@ -21,9 +20,11 @@ requirements = [
 
 setup(
     name='pyRESTcw',
-    version='0.0.9a-dev',
-    packages=['test', 'connectwise', 'connectwise.time', 'connectwise.time.entries', 'connectwise.company',
-              'connectwise.company.contacts', 'connectwise.company.companies', 'connectwise.company.configurations',
+    version='0.0.10a-dev',
+    packages=['test', 'connectwise', 'connectwise.time', 'connectwise.time.entries',
+              'connectwise.company',
+              'connectwise.company.contacts', 'connectwise.company.companies',
+              'connectwise.company.configurations',
               'connectwise.service', 'connectwise.service.boards', 'connectwise.service.tickets',
               'connectwise.service.boardstatuses', 'connectwise.service.priorities'],
     description='Python client library for Connectwise REST API',
@@ -31,17 +32,17 @@ setup(
     url="",
     licence="Apache 2.0",
     platforms="Posix; MacOS X; Windows",
-    setup_requirements = requirements,
-    install_requirements = requirements,
-classifiers=['Development Status :: 5 - Alpha',
-                    'Intended Audience :: Developers',
-                    'License :: OSI Approved :: Apache Software License',
-                    'Operating System :: OS Independent',
-                    'Programming Language :: Python :: 2.7',
-                    'Programming Language :: Python :: 3.2',
-                    'Programming Language :: Python :: 3.4',
-                    'Programming Language :: Python :: 3.6',
-                    'Topic :: Internet',
-                   ]
+    setup_requirements=requirements,
+    install_requirements=requirements,
+    classifiers=['Development Status :: 5 - Alpha',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Internet',
+                 ]
 
 )
